13f77dd25b76d9c868254e19c883babea7f7e26d
sandylol <sandylo@hkbu.edu.hk>
2025-08-29 12:13:49 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo "radxa" | sudo -S sudo apt update
+echo "radxa" | sudo apt install -y git wget uuid-runtime util-linux htop podman uidmap slirp4netns codium btrfs-progs
+
+git clone https://github.com/scie1006/scie1006-rock4se.git
+
+cd scie1006-rock4se
+chmod +x flash-image-to-ssd.sh
+echo "radxa" | sudo -S ./flash-image-to-ssd.sh
+
+
+
+
+
