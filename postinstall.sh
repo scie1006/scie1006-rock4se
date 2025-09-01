6ca63233bc0d0bbd1f57bd49438e36d84f032475
sandylol <sandylo@hkbu.edu.hk>
2025-09-01 15:19:49 +0800

    Create postinstall.sh

---
+++ postinstall.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+
+# install python extension in vs code for all users
+# 
+echo "installing vscode python extensions..."
+sudo code --extensions-dir /usr/share/code/resources/app/extensions --install-extension ms-python.python
+
+#     
+echo "rm ssdm and install pyttsx3..."
+sudo rm /etc/sddm.conf.d/* /usr/share/xsessions/*
+sudo pip install --break-system-packages pyttsx3
+
+# pin applications to taskbar, then run the following to add the pinned application to other users
+echo "pin taskbar application as current user..."
+# pin applications to taskbar, then run the following to add the pinned application to other users
+SOURCE_FILE="/home/sandy/.config/plasma-org.kde.plasma.desktop-appletsrc"
+# Loop through users g01 to g20
+for i in $(seq -w 1 20); do
+    USER="g$i"
+    # Define the target path 
+    TARGET_PATH="/home/$USER/.config/plasma-org.kde.plasma.desktop-appletsrc"
+    #echo "$SOURCE_FILE"
+    #echo "$TARGET_PATH"
+    sudo cp "$SOURCE_FILE" "$TARGET_PATH"
+    # Change the owner of the file to the user
+    sudo chown "$USER":"$USER" "$TARGET_PATH"    
+    echo "Copied $SOURCE_FILE to $TARGET_PATH and changed ownership to $USER."
+done
