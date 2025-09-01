0c82ee5a8120d23d617d55bba7271672f4d74ef8
sandylol <sandylo@hkbu.edu.hk>
2025-09-01 17:28:06 +0800

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -27,7 +27,7 @@ for i in $(seq -w 1 20); do
     
     sudo cp "$SOURCE_FILE" "$TARGET_FILE"
     # Change the owner of the file to the user
-    sudo chown "$USER":"$USER" "$TARGET_FILE"
+    sudo chown -R "$USER":"$USER" "$TARGET_FILE"
     # Inform the user
     echo "Settings file created at: $TARGET_FILE"
 done
